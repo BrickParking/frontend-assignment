53f7cb2c6b4ba014b7200297190b5921eb5d56c1
Ionut Popa <ionutzp@gmail.com>
2019-01-09 11:15:45 +0100

    Added more methods to cross origin allowed requests

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module Myapp
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :options, :put, :delete]
       end
     end
 
